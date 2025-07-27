d0e979583bfbd0f0502c4bdbe59c7e44770747db
ahasanul-shahid <ahasanulshahid@libero.it>
2025-07-27 16:15:26 +0200

    feat: add step for increment/decrement

--- counter/src/app/features/counter/counter.component.ts
+++ counter/src/app/features/counter/counter.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
   counter = 0;
+  stepValue = 1;
   incrementCounter(){
-    this.counter++;
+    this.counter += +this.stepValue;
   }
   decrementCounter(){
-    this.counter--;
+    this.counter -= +this.stepValue;
   }
 }
