62cab7a2be7d38ffff38ef67c81234551f43a9f6
ahasanul-shahid <ahasanulshahid@libero.it>
2025-07-31 13:58:11 +0200

    init guess the number application

---
+++ guess-the-number/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { GuessTheNumberComponent } from './features/guess-the-number/guess-the-number.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [GuessTheNumberComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'guess-the-number';
+}
---
+++ guess-the-number/src/app/features/guess-the-number/guess-the-number.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guess-the-number',
+  imports: [],
+  templateUrl: './guess-the-number.component.html',
+  styleUrl: './guess-the-number.component.scss'
+})
+export class GuessTheNumberComponent {
+
+}
