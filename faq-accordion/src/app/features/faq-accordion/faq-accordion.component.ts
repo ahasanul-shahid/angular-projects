17a927e2d148498a13a5060414ed82fbf710fd55
ahasanul-shahid <ahasanulshahid@libero.it>
2025-08-05 15:35:59 +0200

    init: FAQ accordion application

---
+++ faq-accordion/src/app/features/faq-accordion/faq-accordion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-accordion',
+  imports: [],
+  templateUrl: './faq-accordion.component.html',
+  styleUrl: './faq-accordion.component.scss'
+})
+export class FaqAccordionComponent {
+
+}
