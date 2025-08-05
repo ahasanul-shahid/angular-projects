86c428abe9e8429f55c9a1f7f1d26c3b0143f994
ahasanul-shahid <ahasanulshahid@libero.it>
2025-08-05 16:36:49 +0200

    complete faq-accordion application

---
+++ faq-accordion/src/app/core/data/faq.data.ts
@@ -0,0 +1,17 @@
+export const FAQS = [
+  {
+    question: 'What is Angular?',
+    answer:
+      'Angular is a platform for building mobile and desktop web applications.',
+  },
+  {
+    question: 'What is a component in Angular?',
+    answer:
+      'A component controls a patch of the screen called a view. Components are the main building block of Angular applications.',
+  },
+  {
+    question: 'What are Angular directives?',
+    answer:
+      'Directives are instructions in the DOM. Angular directives allow you to attach behavior to elements in the DOM.',
+  },
+];
--- faq-accordion/src/app/features/faq-accordion/faq-accordion.component.ts
+++ faq-accordion/src/app/features/faq-accordion/faq-accordion.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FAQS } from '../../core/data/faq.data';
 
+interface IFaq {
+  question: string;
+  answer: string;
+}
 @Component({
   selector: 'app-faq-accordion',
   imports: [],
   templateUrl: './faq-accordion.component.html',
-  styleUrl: './faq-accordion.component.scss'
+  styleUrl: './faq-accordion.component.scss',
 })
 export class FaqAccordionComponent {
-
+  faqList: IFaq[] = FAQS;
+  selectedQuestionIndex: number | null = null;
+  onSelect(index: number): void {
+    if (index === this.selectedQuestionIndex) {
+      this.selectedQuestionIndex = null;
+    } else {
+      this.selectedQuestionIndex = index;
+    }
+  }
 }
