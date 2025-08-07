4c8dd071a26c2ae17a127ada660200f9ad3e39b2
ahasanul-shahid <ahasanulshahid@libero.it>
2025-08-07 15:35:38 +0200

    init random joke generator application

---
+++ random-joke-generator/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RandomJokeGeneratorComponent } from './features/random-joke-generator/random-joke-generator.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RandomJokeGeneratorComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'random-joke-generator';
+}
---
+++ random-joke-generator/src/app/features/random-joke-generator/random-joke-generator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-joke-generator',
+  imports: [],
+  templateUrl: './random-joke-generator.component.html',
+  styleUrl: './random-joke-generator.component.scss'
+})
+export class RandomJokeGeneratorComponent {
+
+}
